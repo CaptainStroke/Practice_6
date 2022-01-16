6b2b380c0eff5cd0650776025c3a9db798b850fc
Glib Petrachkov <petrasha@gmail.com>
2022-01-16 13:23:35 +0200

    currency converter

--- Practice_6/Practice_6.cpp
+++ Practice_6/Practice_6.cpp
@@ -159,34 +159,139 @@
 //попыток угадано число, сколько времени это заняло.
 //Предусмотреть выход по 0, в случае, если пользователю надоело угадывать число.
 
+//#include <iostream>
+//#include <ctime>
+//using namespace std;
+//int main()
+//{
+//	srand(time(NULL));
+//	int number = rand() % 500 +1;
+//	time_t start = time(0);
+//	//cout << number << "\n"; // Reveal random number
+//	int guess = 0; 
+//	int Try = 0; // Counter for tries
+//	while (guess != number)
+//	{
+//		cin >> guess;
+//		if (guess > number)
+//		{
+//			cout << " Higher \n";
+//		}
+//		else if (guess < number)
+//		{
+//			cout << " Lower \n";
+//		}
+//		Try++;
+//	}
+//	cout << "You did it in " << Try << " tries and ";
+//	time_t end = time(0);
+//	double time = difftime(end, start);
+//	cout << time << " seconds";
+//}
+
+// Задание 2. Написать программу - конвертер валют.Реализовать общение с пользователем через меню.
+
 #include <iostream>
-#include <ctime>
 using namespace std;
 int main()
 {
-	srand(time(NULL));
-	int number = rand() % 500 +1;
-	time_t start = time(0);
-	//cout << number << "\n"; // Reveal random number
-	int guess = 0; 
-	int Try = 0; // Counter for tries
-	while (guess != number)
+	float money=100;
+	float uah;
+	float usd;
+	float rub;
+	float eur;
+	int choice;
+	int choice2;
+	usd = money; 
+	uah = money / 0.036;
+	eur = money / 1.14;
+	rub = money / 0.013;
+	cout.setf(ios::fixed);
+	cout.precision(4);
+	cout << "Currency exchange. 100 USD\n"; 
+	cout << "CURRENCY  | BUY | SELL \n";
+	cout << " HRYVNYA  | " << uah << " | " << uah + 0.01 * uah << "\n";
+	cout << "US Dollar | " << usd << " | " << usd + 0.01 * usd << "\n";
+	cout << "   EURO   | " << eur << " | " << eur + 0.01 * eur << "\n";
+	cout << "   RUB    | " << rub << " | " << rub + 0.01 * rub << "\n";
+	cout << "Choose your currency from list : \n";
+	cout << "1 - USD | 2 - EUR | 3 - RUB | 4 - UAH \n";
+	cin >> choice;
+	if (choice == 1)
 	{
-		cin >> guess;
-		if (guess > number)
-		{
-			cout << " Higher \n";
-		}
-		else if (guess < number)
-		{
-			cout << " Lower \n";
-		}
-		Try++;
+		usd = money;
+		uah = uah/usd;
+		eur = eur/usd;
+		rub = rub/usd;
+		cout << "Currency - USD\n";
+		cout << "CURRENCY  | BUY | SELL \n";
+		cout << " HRYVNYA  | " << uah << " | " << uah + 0.01 * uah << "\n";
+		cout << "US Dollar | " << usd << " | " << usd + 0.01 * usd << "\n";
+		cout << "   EURO   | " << eur << " | " << eur + 0.01 * eur << "\n";
+		cout << "   RUB    | " << rub << " | " << rub + 0.01 * rub << "\n";
+		//cout << "1 - SELL | 2 - BUY";
 	}
-	cout << "You did it in " << Try << " tries and ";
-	time_t end = time(0);
-	double time = difftime(end, start);
-	cout << time << " seconds";
+	else if (choice == 2)
+	{
+		usd = money/eur;
+		uah = uah/eur;
+		rub = rub / eur;
+		eur = eur / eur;
+		cout << "Currency - EUR\n";
+		cout << "CURRENCY  | BUY | SELL \n";
+		cout << " HRYVNYA  | " << uah << " | " << uah + 0.01 * uah << "\n";
+		cout << "US Dollar | " << usd << " | " << usd + 0.01 * usd << "\n";
+		cout << "   EURO   | " << eur << " | " << eur + 0.01 * eur << "\n";
+		cout << "   RUB    | " << rub << " | " << rub + 0.01 * rub << "\n";
+		//cout << "1 - SELL | 2 - BUY";
+	}
+	else if (choice == 3)
+	{
+		usd = money / rub;
+		uah = uah / rub;
+		eur = eur / rub;
+		rub = rub / rub;
+		cout << "Currency - RUB\n";
+		cout << "CURRENCY  | BUY | SELL \n";
+		cout << " HRYVNYA  | " << uah << " | " << uah + 0.01 * uah << "\n";
+		cout << "US Dollar | " << usd << " | " << usd + 0.01 * usd << "\n";
+		cout << "   EURO   | " << eur << " | " << eur + 0.01 * eur << "\n";
+		cout << "   RUB    | " << rub << " | " << rub + 0.01 * rub << "\n";
+		//cout << "1 - SELL | 2 - BUY";
+	}
+	else if (choice == 4)
+	{
+		usd = money / uah;
+		eur = eur / uah;
+		rub = rub / uah;
+		uah = uah / uah;
+		cout << "Currency - USD\n";
+		cout << "CURRENCY  | BUY | SELL \n";
+		cout << " HRYVNYA  | " << uah << " | " << uah + 0.01 * uah << "\n";
+		cout << "US Dollar | " << usd << " | " << usd + 0.01 * usd << "\n";
+		cout << "   EURO   | " << eur << " | " << eur + 0.01 * eur << "\n";
+		cout << "   RUB    | " << rub << " | " << rub + 0.01 * rub << "\n";
+		//cout << "1 - SELL | 2 - BUY";
+	}
+	else
+	{
+		cout << "Good day. Buy!!! Wellcome again!!\n";
+		return 0;
+	}
+	/*cin >> choice2;
+	if (choice2 == 1)
+	{
+		cout << "SELL\n";
+		cout << "ENTER AMOUNT\n";
+	}
+	else if (choice2 == 2)
+	{
+		cout << "BUY\n";
+		cout << "ENTER AMOUNTY\n";
+	}
+	else
+	{
+		cout << "Good day. Buy!!! Wellcome again!!\n";
+		return 0;
+	}*/
 }
-
-// Задание 2. Написать программу - конвертер валют.Реализовать общение с пользователем через меню.
